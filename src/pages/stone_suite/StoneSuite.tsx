1a2cd9e74f154cb20b57c5e606d56b459f21a274
ameer <daxiameer@gmail.com>
2026-05-13 01:09:03 +0300

    put suite info in assets

--- src/pages/stone_suite/StoneSuite.tsx
+++ src/pages/stone_suite/StoneSuite.tsx
@@ -5,63 +5,12 @@ import SuiteInfo from '../../components/suite_info/SuiteInfo'
 import Prices from '../../components/prices/Prices'
 import '../SuitePage.css'
 import WhatsappButton from '../../components/whatsapp_button/WhatsappButton'
+import SuiteInfoData from '../../assets/suite_info/StoneSuiteInfo'
 
 const imageModules = import.meta.glob("../../assets/images/stone_suite_images/*.jpg", { eager: true })
 const images = Object.values(imageModules).map((module: any) => module.default)
 
-const suiteInfoData = {
-    info: [
-        {
-            label: "מיקום",
-            value: "אזור מישור החוף, דלית אל כרמל"
-        },
-        {
-            label: "ייעוד",
-            value: "זוגות, משפחות, קבוצות, ימי הולדת"
-        },
-        {
-            label: "בריכות",
-            value: ` בריכת שחייה מחוממת ומקורה.
-הבריכה מגודרת. 
-    `}, {
-            label: `המקום מכיל:`,
-            value: `ארון בגדים
-    ספרים לקריאה
-    מיטה זוגית
-    מזגן בכל חדר
-    פינת אוכל
-    מרפסת פרטית עם שולחן וכסאות 
-גקוזי זוגי עגול פונה לנוף
-מטבח מאובזר: פינת קפה, קומקום חשמלי, מקרר, מיקרוגל, סירים, מחבתות, כלי הגשה, כיריים חשמליות.
-חדר רחצה: מגבות, שמפו, תחליב רחצה. 
-`}, {
-            label: "חנייה",
-            value: " 	חנייה חינם. "
-        }, {
-            label: "מפרט החצר",
-            value: `בריכת שחייה מחוממת ומקורה.
-הבריכה מגודרת.
-מדשאה, מנגל, פרגולות, תאורה רומנטית צבעונית לשעות הערב, פרחי נוי, פינת ישיבה מול הנוף. 
-`
-        }, {
-            label: "נוף",
-            value: "נוף גלילי קסום. "
-        },
-        {
-            label: "ויפי",
-            value: "ויפי חינם."
-        },
-        {
-            label: "הערות",
-            value: ` בקרבת המקום: מסעדות, מסלולי טיולים, טרקטורונים, רכיבה על סוסים, גיפים.
-ארוחת ערב בתיאום מראש.
-ארוחת בוקר בתיאום מול המארחים.
-סידור המקום לימי הולדת, הצעות נישואין ושאר אירועים מיוחדים.
-ישנה נגישות לנכים.
-מקבלים בעלי חיים. 
-    `}
-    ]
-}
+const suiteInfoData = SuiteInfoData
 
 const whatsappLink = "https://wa.me/972525338813?text=שלום%20רציתי%20לברר%20פרטים%20על%20סויטת%20אבן"
 

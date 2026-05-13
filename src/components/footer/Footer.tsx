faf63ee6a09b69dfd07401cdf67cf7cae6396fe5
ameer <daxiameer@gmail.com>
2026-05-13 23:58:59 +0300

    added footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>© 2024 צמרת הצימרים. כל הזכויות שמורות.</p>
+                <a href="tel:052-533-8813">צור קשר</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer

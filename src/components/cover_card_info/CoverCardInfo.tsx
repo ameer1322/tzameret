afaf051d57e1a33fde19030aa43929d50f082070
ameer <daxiameer@gmail.com>
2026-07-01 18:21:19 +0300

    removed cover card info

--- src/components/cover_card_info/CoverCardInfo.tsx
+++ src/components/cover_card_info/CoverCardInfo.tsx
@@ -12,7 +12,6 @@ interface CoverCardInfoProps {
 function CoverCardInfo({ info }: CoverCardInfoProps) {
     return (
         <>
-            <h3 className="cover_card_info">Cover Card Info</h3>
             <div className="cover_card_info_rows">
                 {info.map((row, index) => (
                     <div key={index} className="cover_card_info_row">

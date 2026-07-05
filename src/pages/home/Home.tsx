61e858566a2fd1d3d3ab7f94219c5d6aefcf2aa8
ameer1322 <daxiameer@gmail.com>
2026-07-05 06:36:44 +0300

    Update font size Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -28,7 +28,7 @@ function Home() {
 
     return (
         <>
-            <h3 className="welcome">ברוכים הבאים לאתר של צמרת הצימרים</h3>
+            <h2 className="welcome">ברוכים הבאים לאתר של צמרת הצימרים</h3>
             <h3 className="area_title">מתחם עליון</h3>
             <ImageCarousel images={general_upper_images} />
             <div className="cover_cards_container">
